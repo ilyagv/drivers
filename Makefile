7b45c1f6b6855e3f0c01777cff5affd9e1428628
Ilya Gavrilov <ilyagv@gmail.com>
2022-12-07 02:16:05 +0300

    Add module files.

---
+++ Makefile
@@ -0,0 +1,35 @@
+TARGET=test_mod
+
+PWD := $(shell pwd)
+obj-m += ${TARGET}.o
+${TARGET}-y := test_module.o
+#CFLAGS_test_module.o := -DDEBUG
+
+#KDIR := /lib/modules/$(shell uname -r)/build
+KDIR := /home/ilya/share/linux-5.15.71
+
+all:
+	make -C ${KDIR} M=$(PWD) modules
+
+clean:
+	make -C ${KDIR} M=$(PWD) clean
+
+install:
+	insmod ${TARGET}.ko && lsmod | grep ${TARGET}
+
+uninstall:
+	rmmod ${TARGET}
+
+sa_sparse:
+	make clean
+	make C=2 CHECK="/usr/bin/sparse" -C ${KDIR} M=$(PWD) modules
+
+sa_gcc:
+	make clean
+	make W=1 -C ${KDIR} M=$(PWD) modules
+
+sa_flawfinder:
+	make clean
+	flawfinder *.[ch]
+
+.PHONY: clean install uninstall
---
+++ test_module.c
@@ -0,0 +1,32 @@
+#define pr_fmt(fmt) "%s %s:%u " fmt, KBUILD_MODNAME, __func__, __LINE__
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int __init mod_init(void)
+{
+	pr_err("Start. CPU %d\n", smp_processor_id());
+
+	pr_emerg("log-level KERN_EMERG [0]\n");
+	pr_alert("log-level KERN_ALERT [1]\n");
+	pr_crit("log-level KERN_CRIT [2]\n");
+	pr_err("log-level KERN_ERR [3]\n");
+	pr_warn("log-level KERN_WARNING [4]\n");
+	pr_notice("log-level KERN_NOTICE [5]\n");
+	pr_info("log-level KERN_INFO [6]\n");
+	pr_debug("log-level KERN_DEBUG [7]\n");
+	pr_devel("log-level pr_devel [7]\n");
+
+	return 0;
+}
+
+static void __exit mod_exit(void)
+{
+	pr_err("Stop. CPU %d\n", smp_processor_id());
+}
+
+MODULE_LICENSE("GPL");
+
+module_init(mod_init);
+module_exit(mod_exit);
+
