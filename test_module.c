38ffa16f76174175b05287e4206f5f61cf4126c0
Ilya Gavrilov <ilyagv@gmail.com>
2023-01-31 22:54:54 +0300

    Show current process context information.

--- test_module.c
+++ test_module.c
@@ -2,6 +2,36 @@
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/cred.h>
+
+void show_context(const char *name)
+{
+	unsigned int uid = from_kuid(&init_user_ns, current_uid());
+	unsigned int euid = from_kuid(&init_user_ns, current_euid());
+
+	pr_info("%s:%s():%d ", name, __func__, __LINE__);
+	if (likely(in_task())) {
+		pr_info("%s in process context ::\n"
+				" PID         : %6d\n"
+				" TGID        : %6d\n"
+				" UID         : %6d\n"
+				" EUID        : %6d (%s root)\n"
+				" name        : %s\n"
+				" current (ptr to our process context's task_struct) :\n"
+				"               0x%pK (0x%px)\n"
+				" stack start : 0x%pK (0x%px)\n",
+				name,
+				task_pid_nr(current), task_tgid_nr(current),
+				uid, euid,
+				(euid == 0 ? "have" : "don't have"),
+				current->comm,
+				current, current,
+				current->stack, current->stack);
+	} else
+		pr_alert("%s in interrupt context [Should NOT happen here!]\n", name);
+
+}
 
 static int __init mod_init(void)
 {
@@ -17,11 +47,14 @@ static int __init mod_init(void)
 	pr_debug("log-level KERN_DEBUG [7]\n");
 	pr_devel("log-level pr_devel [7]\n");
 
+	pr_info(" sizeof(struct task_struct)=%zd\n", sizeof(struct task_struct));
+	show_context(KBUILD_MODNAME);
 	return 0;
 }
 
 static void __exit mod_exit(void)
 {
+	show_context(KBUILD_MODNAME);
 	pr_err("Stop. CPU %d\n", smp_processor_id());
 }
 
