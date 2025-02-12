98061dc66ac4dba9553e3b466bc6af8c88b450b8
Ilia Gavrilov <Ilia.Gavrilov@infotecs.ru>
2025-02-12 13:58:24 +0300

    Siplify is_metric_better() function.

--- svace_helpers/wifi_mesh/main.c
+++ svace_helpers/wifi_mesh/main.c
@@ -29,16 +29,6 @@
 #include <net/if.h>
 #include <arpa/inet.h>
 
-/*
- #define mult_frac(x, numer, denom)(			\
-{							\
-	typeof(x) quot = (x) / (denom);			\
-	typeof(x) rem  = (x) % (denom);			\
-	(quot * (numer)) + ((rem * (numer)) / (denom));	\
-}							\
-)
-
-*/
 /* Calculate "x * n / d" without unnecessary overflow or loss of precision. */
 #define mult_frac(x, n, d)	\
 ({				\
@@ -60,13 +50,7 @@
 	__builtin_add_overflow(__a, __b, __d);	\
 })
 
-//	printf("%s:%u x %u y %u pesent %u persent_value %u\n"
-//		, __func__, __LINE__
-//		, x, y, persent
-//		, a
-//	      );
-//
-static inline bool is_metric_better(uint32_t x, uint32_t y, const  uint32_t persent)
+static bool is_metric_better_old(uint32_t x, uint32_t y, const  uint32_t persent)
 {
 	uint32_t a, e;
 
@@ -84,6 +68,11 @@ static inline bool is_metric_better(uint32_t x, uint32_t y, const  uint32_t pers
 	}
 
 	return true;
+}
+
+static bool is_metric_better(uint32_t x, uint32_t y)
+{
+	return (x < y) && (x < (y - x / 10));
 }
 
 struct metrics {
@@ -124,6 +113,12 @@ int main(void)
 		{ 1,		8 },
 		{ 1,		9 },
 		{ 1,		10 },
+		{ 2,            3 },
+		{ 3,            4 },
+		{ 4,            5 },
+		{ 7,            4 },
+		{ 6,            4 },
+		{ 5,            4 },
 		{ 10,		11 },
 		{ 10,		12 },
 		{ 10,		13 },
@@ -132,6 +127,9 @@ int main(void)
 		{ 10,		16 },
 		{ 10,		17 },
 		{ 10,		18 },
+		{ 0xffffffff-1, 0xffffffff},
+		{ 1, 0xffffffff},
+		{ 0, 0xffffffff},
 		{ 0xffffffff,	0xffffffff - 1},
 		{ 0xffffffff,	0xffffffff},
 		{ 0xffffffff - 0xffffffff/10 - 1,	0xffffffff},
@@ -162,54 +160,73 @@ int main(void)
 		printf("u64 process=true !!!\n");
 	}
 
-	//printf("num  new_metric             mpath_metric             10%%                          process     u64       u32\n");
-	printf("%3s: %10s(0x%8s) %10s(0x%8s) %10s %10s %10s %7s %7s xxx %7s xxx\n"
+	printf("%3s: %10s(       hex) %10s(       hex) %10s %10s chk %10s %10s   chk %7s %7s %7s %7s xxx %7s xxx\n"
 			, "num"
 			, "new_metric"
-			, ""
 			, "mpath_metr"
-			, ""
-			, "10%"
+			, "10% mult"
+			, "10% div"
 			, "new_me+10%"
 			, "mul_frac64"
+			, "procnew"
 			, "process"
-			, "u64"
+			, "core_10"
+			, "correct"
+			//, "u64"
 			, "u32"
 	      );
+
 	for (i = 0; i < sizeof(test_metrics) / sizeof(test_metrics[0]); ++i) {
 		bool correct = true;
+		bool correct_ten_percent = true;
 		bool u32res = true;
+		bool process_new = true;
 
 		process = true;
+
 		new_metric = test_metrics[i].new_metric;
 		mpath_metric = test_metrics[i].mpath_metric;
 
-		if ( mpath_next_hop != sta ? !is_metric_better(new_metric, mpath_metric, 10) : new_metric >= mpath_metric)
+		if ( mpath_next_hop != sta ? !is_metric_better_old(new_metric, mpath_metric, 10) : new_metric >= mpath_metric)
 			process = false;
+
+		if ( !is_metric_better(new_metric, mpath_metric) )
+			process_new = false;
 
 		if ( (mpath_next_hop != sta ? mult_frac((uint64_t)new_metric, 10, 9) : new_metric) >= mpath_metric)
 			correct = false;
 
+//		uint64_t n = (uint64_t)new_metric + new_metric / 10;
+//		printf("new_metric %u new_metric + 10%% %lu mpath_metric %u >= %u\n"
+//			, new_metric
+//			, n
+//			, mpath_metric
+//			, n >= mpath_metric
+//		      );
+		if ( ((uint64_t)new_metric + new_metric / 10) >= (uint64_t)mpath_metric)
+			correct_ten_percent = false;
+
 		if ( (mpath_next_hop != sta ? mult_frac(new_metric, 10, 9) : new_metric) >= mpath_metric)
 			u32res = false;
 
-//		if (!is_metric_better(new_metric, mpath_metric, 10))
-//			process = false;
-//
-//		if (mult_frac((uint64_t)new_metric, 10, 9) >= mpath_metric)
-//			correct = false;
+		uint32_t ten_percent_mult = mult_frac(new_metric, 10, 100);
+		uint32_t ten_percent_div = new_metric / 10;
 
-		uint32_t persent_value = mult_frac(new_metric, 10, 100);
-		printf("%3u: %10u(0x%08x) %10u(0x%08x) %10u %10lu %10lu %7s %7s %-3s %7s %-3s\n"
+		printf("%3u: %10u(0x%08x) %10u(0x%08x) %10u %10u %-3s %10lu %10lu   %-3s %7s %7s %7s %7s %-3s %7s %-3s\n"
 				, i
 				, new_metric
 				, new_metric
 				, mpath_metric
 				, mpath_metric
-				, persent_value
-				, (uint64_t)new_metric + persent_value
+				, ten_percent_mult
+				, ten_percent_div
+				, ten_percent_div == ten_percent_mult ? "" : "!!!"
+				, (uint64_t)new_metric + ten_percent_div
 				, mult_frac((uint64_t)new_metric, 10, 9)
+				, process == process_new ? "" : "!!!"
+				, bstr(process_new)
 				, bstr(process)
+				, bstr(correct_ten_percent)
 				, bstr(correct)
 				, process == correct ? "" : "!!!"
 				, bstr(u32res)
